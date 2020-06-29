ee27c7284e936745c97e6243cfe8e58f69692a17
Hervé de CHAVIGNY <vevedh@gmail.com>
2020-06-29 16:38:02 -0400

    update

--- electron/plugins/cap-vv-powershell/src/definitions.ts
+++
@@ -1,9 +0,0 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    PowershellPlugin: PowershellPluginPlugin;
-  }
-}
-
-export interface PowershellPluginPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
-}
